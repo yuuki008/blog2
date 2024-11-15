cff7460aa79efdec6c4dc7c076cc019f9243fd10
yuuki008 <s22ah261@gmail.com>
2024-11-15 21:43:58 +0900

    fix author

--- app/(post)/header.tsx
+++ app/(post)/header.tsx
@@ -38,11 +38,11 @@ export function Header({ posts }: { posts: Post[] }) {
           <span className="hidden md:inline">
             <span>
               <a
-                href="https://twitter.com/rauchg"
+                href="https://x.com/nomu487495"
                 className="hover:text-gray-800 dark:hover:text-gray-400"
                 target="_blank"
               >
-                @rauchg
+                @nomu487495
               </a>
             </span>
 
